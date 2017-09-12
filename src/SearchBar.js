c8877d1ec5573a1ce9fb0d28e2aac701dcbeb7c5
Ruslan <khamatdinovrt@bashedu.ru>
2017-09-12 16:31:29 +0500

    added throttle function

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -4,12 +4,22 @@ import TextField from 'material-ui/TextField';
 import { API_URL } from './index';
 
 const SearchBar = ({ fetchVacancies, searchText, textToSearch }) => {
-    const handleChange = (e, newValue) => {
-        setTimeout(() => {
+    const throttle = (callback, ms) => {
+        var timer = null;
+        return function () {
+            let args = arguments;
+            clearTimeout(timer);
+            timer = setTimeout(() => {
+                callback.apply(this, args);
+            }, ms);
+        };
+    };
+      
+    const handleChange = throttle((e, newValue) => {
             searchText(newValue)
             fetchVacancies(API_URL + `&text=${newValue}`)
         }, 500)
-    }
+    
     
     return (
         <Paper className='search-block' zDepth={1}>
